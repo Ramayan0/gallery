59a3cdfe7f93fd83dbf3ecf11e4df17f44e6340d
Ramayan0 <hafieekhan@gmail.com>
2024-11-25 06:31:47 +0300

    Add MILESTONE 2 to the landing page

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let index = require('./routes/index');
 let image = require('./routes/image');
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
+let mongodb_url = 'mongodb+srv://hamsaadan1:Ramayan6506@project-1cluster.crlkc.mongodb.net/?retryWrites=true&w=majority&appName=Project-1Cluster';
 let dbName = 'darkroom';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
